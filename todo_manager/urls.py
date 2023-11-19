fa2f918c8a70515d981945da9912d56bf841eeb6
serhii_loboda <serhiy.loboda@gmail.com>
2023-11-19 17:12:13 +0200

    install flake8

--- todo_manager/urls.py
+++ todo_manager/urls.py
@@ -15,9 +15,18 @@ from todo_manager.views import (
 urlpatterns = [
     path("", TaskListView.as_view(), name="task-list"),
     path("create/", TaskCreateView.as_view(), name="task-create"),
-    path("task/<int:pk>/update/", TaskUpdateView.as_view(), name="task-update"),
-    path("task/<int:pk>/delete/", TaskDeleteView.as_view(), name="task-delete"),
-    path("task/<int:pk>/completing-task", completing_task, name="completing-task"),
+    path(
+        "task/<int:pk>/update/",
+        TaskUpdateView.as_view(), name="task-update"
+    ),
+    path(
+        "task/<int:pk>/delete/",
+        TaskDeleteView.as_view(), name="task-delete"
+    ),
+    path(
+        "task/<int:pk>/completing-task",
+        completing_task, name="completing-task"
+    ),
     path("tags/", TagListView.as_view(), name="tag-list"),
     path("tags/create/", TagCreateView.as_view(), name="tag-create"),
     path("tags/<int:pk>/update/", TagUpdateView.as_view(), name="tag-update"),
